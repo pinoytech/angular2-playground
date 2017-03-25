4934570aa44d1879e180e9db922871b74c9d7b41
Thorpe Obazee <forum.providence@yahoo.com>
2017-03-25 08:51:57 +0800

    Adding Routing Adding Guards

--- pl-apm/apm/app/app.module.ts
+++ pl-apm/apm/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductDetailGuard } from './products/product-guard.service';
+import { WelcomeComponent } from './home/welcome.component';
 import { StarComponent } from './shared/star.component';
 
 import { ProductFilterPipe } from './products/product-filter.pipe';
@@ -13,13 +17,27 @@ import { ProductFilterPipe } from './products/product-filter.pipe';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', component: WelcomeComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductFilterPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
+  ],
+  providers: [
+    ProductDetailGuard
   ],
   bootstrap: [AppComponent]
 })
